2fe22b53682bd2c4843eeaf8a2ba9d292637467b
habdil <habdiliqrawardana@gmail.com>
2024-09-28 17:37:00 +0700

    setup project management

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello World</p>
+
+      <Button>
+        Click Me
+      </Button>
+
+    </div>
+  )
+}
